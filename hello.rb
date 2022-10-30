7260d51b56bcad9712d6de049551247ee458d440
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-10-30 17:54:06 +0900

    練習

--- hello.rb
+++ hello.rb
@@ -1,2 +1,23 @@
-puts "hello ruby"
-puts "putsメソッドは値を出力することができるメソッドです"
+users = {
+  name: "Takuya",
+  height: 175,
+  weight: 60,
+  gender: "男"
+  }
+  
+  # ハッシュを出力する
+  p users
+  puts "----------"
+  
+  # usersハッシュに:telで新たにオブジェクトを追加する
+  users[:tel] = "0801234567"
+  
+  # ハッシュを出力する
+  p users
+  puts "----------"
+  
+  # ハッシュの要素を削除する
+  users.delete(:weight)
+  
+  # ハッシュを出力する
+  p users
