94bea4874c06b6f2a739efd6973104a2873fc0f8
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-11-07 11:18:54 +0900

    test

---
+++ ruby_nyuumon.rb
@@ -0,0 +1,26 @@
+q1 = 2 + 3
+puts q1
+
+#Q2
+r = 2.0
+ra = r * r * 3.14
+puts ra
+
+#Q3
+puts = "Ruby"
+
+#Q4
+puts "abc" + "def"
+
+#Q5
+q5 = "123"
+q5a = "456"
+puts q5.to_i + q5a.to_i
+
+#Q6
+coffee = 300
+esplesso = 100*2
+sum = coffee + esplesso
+
+puts = "コーヒー：円"
+puts = "合計：#{sum}円"
