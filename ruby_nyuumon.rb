f0e47d3a1307975efb71dd1acaf50773201d76b5
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-11-12 21:05:57 +0900

    test

--- ruby_nyuumon.rb
+++ ruby_nyuumon.rb
@@ -1,26 +1,16 @@
-q1 = 2 + 3
-puts q1
+class Item
+  def name
+    @name
+  end
+  def name=(text)
+    @name = text
+  end
+end
 
-#Q2
-r = 2.0
-ra = r * r * 3.14
-puts ra
+class Food < Item
+end
 
-#Q3
-puts = "Ruby"
+test = Food.new
+a = test.name=("チーズケーキ") 
 
-#Q4
-puts "abc" + "def"
-
-#Q5
-q5 = "123"
-q5a = "456"
-puts q5.to_i + q5a.to_i
-
-#Q6
-coffee = 300
-esplesso = 100*2
-sum = coffee + esplesso
-
-puts = "コーヒー：円"
-puts = "合計：#{sum}円"
+puts a
